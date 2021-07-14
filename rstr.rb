d44c272d36d70536ffad5a8b0aa906771e03d33e
Giuseppe Papallo <giuseppe@evolution.ai>
2021-07-14 19:53:21 -0300

    update sha256 of rstr formula

--- rstr.rb
+++ rstr.rb
@@ -3,7 +3,7 @@ class Rstr < Formula
   desc "A simple content addressable blob store with a web interface."
   homepage "https://github.com/giuppep/rstr"
   url "https://github.com/giuppep/rstr/releases/download/#{version}/rstr-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "0227757bf1c5ecbddbf12d7576f394ed842d08669ccbd25b386b051a29c4d5ba"
+  sha256 "1a25329fa4a8e50e05ad592a0e8291484c4d06f9b7d88351cc85bb59ab026663"
 
   def install
     bin.install "rstr"
